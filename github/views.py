f8f465b4147ffe01e5800226895cbffc46d787f7
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2012-03-30 16:11:19 -0600

    copiar los archivos estaticos

--- github/views.py
+++ github/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.conf import settings
 
 import commands
 
 def update(solicitud):
-	return HttpResponse(commands.getstatusoutput('git pull')[1])
+	gitpull = commands.getstatusoutput('git pull')[1]
+	commands.getstatusoutput('cp -R static/* %s' % settings.HTTPD_STATIC)[1]
+	return HttpResponse(gitpull)
