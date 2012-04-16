f823c3fd936f50aaee9afd470cb19632f182130a
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2012-04-16 17:09:08 -0500

    arreglos en el feed

--- static/js/admin.js
+++ static/js/admin.js
@@ -4,5 +4,6 @@ tinyMCE.init({
 	theme : "advanced",
 	theme_advanced_toolbar_location: "top",
 	theme_advanced_toolbar_align: "left",
-	forced_root_block: ''
+	forced_root_block: '',
+	height:300
 });
